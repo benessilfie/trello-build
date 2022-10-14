73f458ff8dd40871e2885e51fcf8017ed055beef
essilfiequansah <bquansah007@gmail.com>
2022-10-14 02:29:27 +0000

    fix drag implementation for the column component

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -14,7 +14,7 @@ export function insertItemAtIndex<TItem>(array: TItem[], item: TItem, index: num
   return [...array.slice(0, index), item, ...array.slice(index)];
 }
 
-export function moveItem<TItem>(array: TItem[], from: number, to: number) {
+export const moveItem = <TItem>(array: TItem[], from: number, to: number) => {
   const item = array[from];
   return insertItemAtIndex(removeItemAtIndex(array, from), item, to);
-}
+};
