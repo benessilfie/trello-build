8e66fc86c810509311b04638643931cdeb021093
essilfiequansah <bquansah007@gmail.com>
2022-10-14 11:09:37 +0000

    add drag implementaion for the card component

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -21,6 +21,10 @@ export type AppState = {
 
 export const appStateReducer = (draft: AppState, action: Action): AppState | void => {
   switch (action.type) {
+    case "SET_DRAGGED_ITEM": {
+      draft.draggedItem = action.payload;
+      break;
+    }
     case "ADD_LIST": {
       draft.lists.push({
         id: nanoid(),
@@ -46,8 +50,25 @@ export const appStateReducer = (draft: AppState, action: Action): AppState | voi
       draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
       break;
     }
-    case "SET_DRAGGED_ITEM": {
-      draft.draggedItem = action.payload;
+    case "MOVE_TASK": {
+      const { draggedItemId, hoveredItemId, sourceColumnId, targetColumnId } = action.payload;
+
+      const sourceListIndex = findItemIndexById(draft.lists, sourceColumnId);
+
+      const targetListIndex = findItemIndexById(draft.lists, targetColumnId);
+
+      const dragIndex = findItemIndexById(draft.lists[sourceListIndex].tasks, draggedItemId);
+
+      const hoverIndex = hoveredItemId
+        ? findItemIndexById(draft.lists[targetListIndex].tasks, hoveredItemId)
+        : 0;
+
+      const item = draft.lists[sourceListIndex].tasks[dragIndex];
+
+      draft.lists[sourceListIndex].tasks.splice(dragIndex, 1);
+
+      draft.lists[targetListIndex].tasks.splice(hoverIndex, 0, item);
+
       break;
     }
     default: {
