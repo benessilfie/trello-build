4a31db29ab8636c42f7b649531a0f97fe050faf1
essilfiequansah <bquansah007@gmail.com>
2022-10-14 00:32:29 +0000

    implement global state

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,8 +2,9 @@ import { CardContainer } from "./styles";
 
 type CardProps = {
   text: string;
+  id: string;
 };
 
 export const Card = ({ text }: CardProps) => {
-  return <CardContainer >{text}</CardContainer>;
+  return <CardContainer>{text}</CardContainer>;
 };
---
+++ src/state/AppStateContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, useContext, FC } from "react";
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+const addData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Generate app scaffold" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "c2", text: "Learn Typescript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "Begin to use static typing" }],
+    },
+  ],
+};
+
+type AppStateContextProps = {
+  lists: List[];
+  getTasksByListId(id: string): Task[];
+};
+
+type AppStateProviderProps = {
+  children: React.ReactNode;
+};
+
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps);
+
+export const AppStateProvider: FC<AppStateProviderProps> = ({ children }) => {
+  const { lists } = addData;
+
+  const getTasksByListId = (id: string) => {
+    return lists.find((list) => list.id === id)?.tasks || [];
+  };
+
+  return (
+    <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+export const useAppState = () => {
+  return useContext(AppStateContext);
+};
